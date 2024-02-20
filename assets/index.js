59c18c2bb88c4b845580123d6b37b29890c56f3d
bhimlex13 <oroalexander004@gmail.com>
2024-02-20 22:29:42 +0900

    add info management sample questions

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+// Disable right-click context menu
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+  
+  // Disable certain keyboard shortcuts
+  document.onkeydown = function(e) {
+    // Disable F12
+    if (e.key == 123) {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+I
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+C
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+J
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+U
+    if (e.ctrlKey && e.key == 'U') {
+      e.preventDefault();
+    }
+  };
