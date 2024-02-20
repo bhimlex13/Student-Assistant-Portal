eca5a2e8a85c84a1ab6361aa630c92f54f8dafc2
Alex <oroalexander004@gmail.com>
2024-02-20 11:50:51 +0800

    add asset as general for style and script

---
+++ net_tech_1/prelims/data.js
@@ -0,0 +1,53 @@
+// This is the data for the quiz questions, answers, and choices
+var quizData = [
+  {
+    "question": "It consists of two insulated strands of copper wire that are arranged in a regular spiral pattern.",
+    "answer": "Twisted-Pair",
+    "choices": ["Twisted-Pair", "Serial", "Coaxial", "Fiber Optic"]
+  },
+  {
+    "question": "It is a connection wherein data flows in one (1) direction only (unidirectional).",
+    "answer": "Simplex",
+    "choices": ["Simplex", "Half-duplex", "Full-duplex", "Complex"]
+  },
+  {
+    "question": "It describes the position of the waveform relative to time zero.",
+    "answer": "Phase",
+    "choices": ["Phase", "Frequency", "Period", "Wavelength"]
+  },
+  {
+    "question": "Which of the following is NOT a definition of a signal?",
+    "answer": "A metallic structure that acts as a transducer.",
+    "choices": ["A metallic structure that acts as a transducer.", "Means of communication.", "Communicated information.", "Electronics transmitted information."]
+  },
+  {
+    "question": "It refers to a vertical distance from a given point on the wave from the horizontal axis.",
+    "answer": "Amplitude",
+    "choices": ["Amplitude", "Wave", "Wavelength", "Phase"]
+  },
+  {
+    "question": "It is a connection wherein data simultaneously flows in both directions.",
+    "answer": "Full-duplex",
+    "choices": ["Full-duplex", "Simplex", "Half-duplex", "Complex"]
+  },
+  {
+    "question": "It refers to the number of waves made per second or as cycles per second.",
+    "answer": "Frequency",
+    "choices": ["Frequency", "Phase", "Period", "Wavelength"]
+  },
+  {
+    "question": "It operates over distances up to 50 feet and has a communication speed equal to 20Kbps.",
+    "answer": "Serial",
+    "choices": ["Serial", "Twisted-Pair", "Coaxial", "Fiber Optic"]
+  },
+  {
+    "question": "It is a connection wherein data can flow in both directions, but not simultaneously over a shared physical medium.",
+    "answer": "Half-duplex",
+    "choices": ["Half-duplex", "Simplex", "Full-duplex", "Complex"]
+  },
+  {
+    "question": "It refers to the horizontal distance of a wave from a point to the corresponding point on the next wave.",
+    "answer": "Wavelength",
+    "choices": ["Wavelength", "Amplitude", "Phase", "Period"]
+  }
+];
