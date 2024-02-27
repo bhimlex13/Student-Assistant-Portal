c0c517f714c9d340a4e0bdeb378375f35815396b
Alex <oroalexander004@gmail.com>
2024-02-27 11:02:47 +0800

    chenge questions to dummy questions

--- inte_prog/prelims/data.js
+++ inte_prog/prelims/data.js
@@ -1,88 +1,51 @@
 // This is the data for the quiz questions, answers, and choices
 var quizData = [
-    {
-      "question": "The development of C# was headed by which individual at Microsoft?",
-      "answer": "Anders Hejlsberg",
-      "choices": ["Bill Gates", "Steve Jobs", "Anders Hejlsberg", "Linus Torvalds"]
-    },
-    {
-      "question": "Which programming paradigm is primarily associated with C#?",
-      "answer": "Object-oriented programming",
-      "choices": ["Procedural programming", "Object-oriented programming", "Scripting", "Markup"]
-    },
-    {
-      "question": "Multiple applications can use a shared assembly.",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "By default, all assemblies are considered \"public\".",
-      "answer": "FALSE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "Each assembly has a 64-bit version number that is presented as a set of four (4) decimal pieces.",
-      "answer": "FALSE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "There are three (3) types of assemblies, namely: private, shared, and password protected.",
-      "answer": "FALSE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": ".NET uses an assembly for code sharing which only allows authorized categories of users or processes to call designated methods of specific classes.",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "VB, C++, C#, Jscript, and J# are languages supported by .NET",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "It is a component of the .NET framework which controls tasks and manages system resources of the computer.",
-      "answer": "Operating System",
-      "choices": ["Operating System", "Common Language Runtime", "ADO.NET", "ASP.NET"]
-    },
-    {
-      "question": "It is a component of the assembly that provides information along with a complete description of methods, types, and other resources.",
-      "answer": "Metadata",
-      "choices": ["Methods", "Metadata", "LINQ", "Class"]
-    },
-    {
-      "question": "It is a formal specification that documents how types are declared, used, and managed so that the Common Language Runtime can use them.",
-      "answer": "Common Type System",
-      "choices": ["Common Language Specification", "Common Type System", ".NET Base Class Library", "ADO.NET"]
-    },
-    {
-      "question": "ADO.NET provides access to relational databases and several data sources such as SQL Server and XML.",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "XML allows developers to easily describe and deliver rich and structured data from any application in a standard and consistent way.",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "The Common Language Specification controls tasks and manages resources of the computer.",
-      "answer": "FALSE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "The Common Language Runtime converts the source code into an intermediate language.",
-      "answer": "TRUE",
-      "choices": ["TRUE", "FALSE"]
-    },
-    {
-      "question": "Which of the following is NOT a feature of C#?<br><br>It includes a large library and supports several programming languages<br>It supports garbage collection and automatic memory management<br>It produces portable code<br>Its programs execute in a secure, controlled runtime environment",
-      "answer": "It includes a large library and supports several programming languages",
-      "choices": [
-        "It includes a large library and supports several programming languages",
-        "It supports garbage collection and automatic memory management",
-        "It produces portable code",
-        "Its programs execute in a secure, controlled runtime environment"
-      ]
-    }
-  ];
+  {
+    "question": "Question Sample 1 (Answer is A)",
+    "answer": "A",
+    "choices": [
+      "A",
+      "B",
+      "C",
+      "D"
+    ],
+    "reference": "01 Handout 1",
+    "term": "Prelim"
+  },
+  {
+    "question": "Question Sample 1 (Answer is B)",
+    "answer": "B",
+    "choices": [
+      "A",
+      "B",
+      "C",
+      "D"
+    ],
+    "reference": "01 Handout 1",
+    "term": "Prelim"
+  },
+  {
+    "question": "Question Sample 1 (Answer is C)",
+    "answer": "C",
+    "choices": [
+      "A",
+      "B",
+      "C",
+      "D"
+    ],
+    "reference": "01 Handout 1",
+    "term": "Prelim"
+  },
+  {
+    "question": "Question Sample 1 (Answer is D)",
+    "answer": "D",
+    "choices": [
+      "A",
+      "B",
+      "C",
+      "D"
+    ],
+    "reference": "01 Handout 1",
+    "term": "Prelim"
+  }
+]
