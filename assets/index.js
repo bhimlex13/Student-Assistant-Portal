b4a0a41441fe1fa44e0e99bfd7253be2365156ba
ElmerF5445 <contentbyelmerf@gmail.com>
2024-02-28 00:47:29 +0800

    Added Sign In window that pops up in index.html; Added JS functionality to disable Sign In on index.html; Layout may and will change

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,43 @@
-// function run(){
-//     var password = prompt("Password Please");
-//     if(password != '1234'){
-//     document.body.innerHTML = '';
-//     document.body.innerHTML = 'Password Failed! Reload to Renter Password';
-//     }else{
-//     alert('Success');
-//     }
-//     }
-//     run();
+let SignIn_Credentials = {
+  // Set to false to prevent the sign in window to appear
+  RequireSignIn: true, 
+  // Password for said sign in window
+  Password: '1234' 
+}
+
+function run(){
+  document.getElementById("SignIn-Dialog-Content-Body-Form-Password").value = "";
+  var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+  myModal.show();
+}
+
+// When the page is loaded, open the sign in window
+window.onload = function (){
+  if (SignIn_Credentials.RequireSignIn == true){
+    run();
+  }
+}
+
+
+// When the password is submitted, run this function
+document.getElementById("SignIn-Dialog-Content-Body-Form").addEventListener("submit", function(event){
+  // Prevents default submit action
+  event.preventDefault();
+  // Rehides the error message
+  document.getElementById("SignIn-Dialog-Content-Body-Form-Password-Error").style.display = "none";
+  // Gets the password value
+  var password = document.getElementById("SignIn-Dialog-Content-Body-Form-Password").value;
+  // Checks if the password is correct
+  if(password != SignIn_Credentials.Password) {
+    // Password incorrect - Show error message
+    document.getElementById("SignIn-Dialog-Content-Body-Form-Password").value = "";
+    document.getElementById("SignIn-Dialog-Content-Body-Form-Password-Error").style.display = "block";
+  } else {
+    // Password correct - Close window
+    document.getElementById("SignIn-Dialog-Content-Close").click();
+  }
+});
+
 
 
 // Disable right-click context menu
