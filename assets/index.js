4541e7c62f8451db696ddfd73580dedf6cf39264
Alex <oroalexander004@gmail.com>
2024-02-29 10:57:27 +0800

    Verbs gumana ka verbs

--- assets/index.js
+++ assets/index.js
@@ -61,7 +61,7 @@ document.getElementById("SignIn-Dialog-Content-Body-Form").addEventListener("sub
 // Object that contains the subject list
 let SubjectList = {};
 // URL to the .json file 
-let SubjectList_ManifestFileURL = "./SAP-Subject-Manifest.json";
+var SubjectList_ManifestFileURL = "assets/SAP-Subject-Manifest-Test.json";
 
 // Gets the manifest file and returns the data
 async function SubjectList_FetchManifest(URL) {
