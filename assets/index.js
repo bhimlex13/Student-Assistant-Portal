a9fa9fc10de833726ae7be075f3f3ad5211de300
bhimlex13 <oroalexander004@gmail.com>
2024-03-08 08:40:26 +0900

    add second handout

--- assets/index.js
+++ assets/index.js
@@ -190,31 +190,31 @@ function SubjectList_SwitchScreenTo(Screen){
 }
 
 // Disable right-click context menu
-// document.addEventListener('contextmenu', function (e) {
-//   // alert('⚠️ Sorry. Di muna pwede mag right click here ⚠️')
-//   e.preventDefault();
-// });
+document.addEventListener('contextmenu', function (e) {
+  // alert('⚠️ Sorry. Di muna pwede mag right click here ⚠️')
+  e.preventDefault();
+});
   
-  // // Disable certain keyboard shortcuts
-  // document.onkeydown = function(e) {
-  //   // Disable F12
-  //   if (e.key == 123) {
-  //     e.preventDefault();
-  //   }
-  //   // Disable Ctrl+Shift+I
-  //   if (e.ctrlKey && e.shiftKey && e.key == 'I') {
-  //     e.preventDefault();
-  //   }
-  //   // Disable Ctrl+Shift+C
-  //   if (e.ctrlKey && e.shiftKey && e.key == 'C') {
-  //     e.preventDefault();
-  //   }
-  //   // Disable Ctrl+Shift+J
-  //   if (e.ctrlKey && e.shiftKey && e.key == 'J') {
-  //     e.preventDefault();
-  //   }
-  //   // Disable Ctrl+U
-  //   if (e.ctrlKey && e.key == 'U') {
-  //     e.preventDefault();
-  //   }
-  // };
+  // Disable certain keyboard shortcuts
+  document.onkeydown = function(e) {
+    // Disable F12
+    if (e.key == 123) {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+I
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+C
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+Shift+J
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+      e.preventDefault();
+    }
+    // Disable Ctrl+U
+    if (e.ctrlKey && e.key == 'U') {
+      e.preventDefault();
+    }
+  };
