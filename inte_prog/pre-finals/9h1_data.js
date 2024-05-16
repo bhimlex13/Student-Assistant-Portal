92e3f658104c609e88052776dd059974d2546d87
bhimlex13 <oroalexander004@gmail.com>
2024-05-17 04:58:11 +0900

    add questions for SIA and Integ

---
+++ inte_prog/pre-finals/9h1_data.js
@@ -0,0 +1,185 @@
+// This is the data for the quiz questions, answers, and choices
+var quizData = [
+  {
+    "question": "It is a group of statement that accomplishes a specific task.",
+    "answer": "Method",
+    "choices": [
+      "Method",
+      "Class",
+      "Namespace",
+      "Variable"
+    ]
+  },
+  {
+    "question": "What determines the access level or visibility of the method from another class?",
+    "answer": "Access Modifier",
+    "choices": [
+      "Access Modifier",
+      "Class",
+      "Namespace",
+      "Parameters"
+    ]
+  },
+  {
+    "question": "It specifies whether the method returns a value or not.",
+    "answer": "Return Type",
+    "choices": [
+      "Return Type",
+      "Access Modifier",
+      "Method",
+      "Parameters"
+    ]
+  },
+  {
+    "question": "\"public void printRectArea(int width, int height)\" is an example of what?",
+    "answer": "Method Signatures",
+    "choices": [
+      "Method Signatures",
+      "Method Syntax",
+      "Method Invoking",
+      "Method Creation"
+    ]
+  },
+  {
+    "question": "In a method, what are the values inside the parenthesis called?",
+    "answer": "Parameters",
+    "choices": [
+      "Parameters",
+      "Variables",
+      "Method Name",
+      "Data Type"
+    ]
+  },
+  {
+    "question": "\"SampleClass obj1 = new SampleClass();\" is the correct syntax for creating an object.",
+    "answer": "The statement is TRUE .",
+    "choices": [
+      "The statement is TRUE .",
+      "The statement is FALSE ."
+    ]
+  },
+  {
+    "question": "__________ means that methods of the same name can be declared in the same \r\nclass as long as they have different sets of parameters which are determined by the number, types, and order of the parameters.",
+    "answer": "Method Overloading",
+    "choices": [
+      "Method Overloading",
+      "Method Creating",
+      "Method Modifying",
+      "Method Invoking"
+    ]
+  },
+  {
+    "question": "These are used to make a single variable that holds related data of \r\nvarious data types",
+    "answer": "Structures",
+    "choices": [
+      "Structures",
+      "Class",
+      "Method",
+      "Interface"
+    ]
+  },
+  {
+    "question": "\"Sample obj1.create = new Sample\" is the correct syntax to create an object.",
+    "answer": "The statement is FALSE .",
+    "choices": [
+      "The statement is TRUE .",
+      "The statement is FALSE ."
+    ]
+  },
+  {
+    "question": "Structures can also contain constructors and methods.",
+    "answer": "The statement is TRUE .",
+    "choices": [
+      "The statement is TRUE .",
+      "The statement is FALSE ."
+    ]
+  },
+  {
+    "question": "What is a data structure or blueprint that defines data and actions in a single unit?",
+    "answer": "Class",
+    "choices": [
+      "Method",
+      "Class",
+      "Namespace",
+      "Variable"
+    ]
+  },
+  {
+    "question": "This is the process of hiding or encapsulating data from the outside world.",
+    "answer": "Encapsulation",
+    "choices": [
+      "Abstraction",
+      "Encapsulation",
+      "Inheritance",
+      "Polymorphism"
+    ]
+  },
+  {
+    "question": "A class or structure cannot specify how \r\naccessible each of its members is to code outside of the class or structure.",
+    "answer": "The statement is FALSE .",
+    "choices": [
+      "The statement is TRUE .",
+      "The statement is FALSE ."
+    ]
+  },
+  {
+    "question": "Which accessor ensures that the instance variable is not directly accessible?",
+    "answer": "Private",
+    "choices": [
+      "Public",
+      "Private",
+      "Final",
+      "Protected"
+    ]
+  },
+  {
+    "question": "Which Accessor uses the keyword \"return?\"",
+    "answer": "Get",
+    "choices": [
+      "Set",
+      "Get",
+      "This",
+      "Object"
+    ]
+  },
+  {
+    "question": "Which among the following allows the changing of value in encapsulation?",
+    "answer": "Set",
+    "choices": [
+      "Set",
+      "Get",
+      "This",
+      "Object"
+    ]
+  },
+  {
+    "question": "The ____ keyword refers to the object or the current instance of the class.",
+    "answer": "This",
+    "choices": [
+      "Set",
+      "Get",
+      "This",
+      "Object"
+    ]
+  },
+  {
+    "question": "This is a special method where its name is the same as the name of its type.",
+    "answer": "Constructor",
+    "choices": [
+      "Constructor",
+      "Class",
+      "Structure",
+      "Interface"
+    ]
+  },
+  {
+    "question": "What keyword should be used in order to access the contents of other namespaces?",
+    "answer": "Using",
+    "choices": [
+      "Using",
+      "Set",
+      "Get",
+      "This"
+    ]
+  }
+]
