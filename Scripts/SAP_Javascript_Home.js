5953d57b28e22f5fcf8994be333192581621b52d
ElmerF5445 <contentbyelmerf@gmail.com>
2024-08-13 22:35:40 +0800

    Completed Quiz Explorer on Home.html

--- Scripts/SAP_Javascript_Home.js
+++ Scripts/SAP_Javascript_Home.js
@@ -59,58 +59,83 @@ async function Quizzes_Manifest_Load(Quizzes_Manifest_FileURL) {
 
 // Subject array
 function Quizzes_Explorer_Load_Subject(){
-    Explorer_Container_Clear();
-    Explorer_Container_ChangeType("Image");
-    Element_Attribute_Set("Quizzes_Explorer", "Level", "1");
-    for (a = 0; a < Quizzes_Manifest.Subject.length; a++){
-        let Object = Quizzes_Manifest.Subject[a];
-        if (Object.Subject_Status == "Active"){
-            Explorer_Item_Create(Object.Subject_ID, Object.Subject_Name, null, Object.Subject_Thumbnail, null, `Quizzes_Explorer_Load_Module(${a})`);
+    Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Close", "0.2s", "forwards", "1", 0);
+    setTimeout(function(){
+        Element_Attribute_Set("Quizzes_Container", "State", "Header_Inactive");
+        Explorer_Container_Clear();
+        Explorer_Container_ChangeType("Image");
+        Element_Attribute_Set("Quizzes_Explorer", "Level", "1");
+        for (a = 0; a < Quizzes_Manifest.Subject.length; a++){
+            let Object = Quizzes_Manifest.Subject[a];
+            if (Object.Subject_Status == "Active"){
+                Explorer_Item_Create(Object.Subject_ID, Object.Subject_Name, null, Object.Subject_Thumbnail, null, `Quizzes_Explorer_Load_Module(${a})`);
+            }
+            
         }
-        
-    }
+        Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Open", "0.5s", "forwards", "1", 100);
+    }, 200);
+    
 }
 
 // Module array
 function Quizzes_Explorer_Load_Module(Item){
-    Explorer_Container_Clear();
-    Explorer_Container_ChangeType("Image");
-    Element_Attribute_Set("Quizzes_Explorer", "Level", "2");
-    Element_Attribute_Set("Quizzes_Explorer", "Memory_Value", Item);
-    
-    for (a = 0; a < Quizzes_Manifest.Subject[Item].Subject_Module.length; a++){
-        let Object = Quizzes_Manifest.Subject[Item].Subject_Module[a];
-        if (Object.Module_Status == "Active"){
-            Explorer_Item_Create(Object.Module_ID, Object.Module_Name, Object.Module_LastModified, Object.Module_Thumbnail, null, `Quizzes_Explorer_Load_Quizzes(${Item}, ${a})`);
+    Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Close", "0.2s", "forwards", "1", 0);
+    setTimeout(function(){
+        Element_Attribute_Set("Quizzes_Container", "State", "Header_Active");
+        Element_Attribute_Set("Quizzes_Title_Content", "Stage", "Subject");
+        Explorer_Container_Clear();
+        Explorer_Container_ChangeType("Image");
+        Element_Attribute_Set("Quizzes_Explorer", "Level", "2");
+        Element_Attribute_Set("Quizzes_Explorer", "Memory_Value", Item);
+        
+        for (a = 0; a < Quizzes_Manifest.Subject[Item].Subject_Module.length; a++){
+            let Object = Quizzes_Manifest.Subject[Item].Subject_Module[a];
+            if (Object.Module_Status == "Active"){
+                Explorer_Item_Create(Object.Module_ID, Object.Module_Name, Object.Module_LastModified, Object.Module_Thumbnail, null, `Quizzes_Explorer_Load_Quizzes(${Item}, ${a})`);
+            }
         }
-    }
+
+        document.getElementById("Quizzes_Title_Subject").innerHTML = Quizzes_Manifest.Subject[Item].Subject_Name;
+        Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Open", "0.5s", "forwards", "1", 100);
+    }, 200);
+    
 }
 
 // Subfolder array
 function Quizzes_Explorer_Load_Quizzes(Item, Module){
-    Explorer_Container_Clear();
-    Explorer_Container_ChangeType("Text");
-    Element_Attribute_Set("Quizzes_Explorer", "Level", "3");
-    for (a = 0; a < Quizzes_Manifest.Subject[Item].Subject_Module[Module].Module_Subfolders.length; a++){
-        let Object = Quizzes_Manifest.Subject[Item].Subject_Module[Module].Module_Subfolders[a];
-        if (Object.Subfolder_Status == "Active"){
-            Explorer_Item_Create(Object.Subfolder_ID, Object.Subfolder_Name, Object.Subfolder_LastModified, null, Object.Subfolder_Link, null);
+    Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Close", "0.2s", "forwards", "1", 0);
+    setTimeout(function(){
+        Element_Attribute_Set("Quizzes_Container", "State", "Header_Active");
+        Element_Attribute_Set("Quizzes_Title_Content", "Stage", "Folder");
+        Explorer_Container_Clear();
+        Explorer_Container_ChangeType("Text");
+        Element_Attribute_Set("Quizzes_Explorer", "Level", "3");
+        for (a = 0; a < Quizzes_Manifest.Subject[Item].Subject_Module[Module].Module_Subfolders.length; a++){
+            let Object = Quizzes_Manifest.Subject[Item].Subject_Module[Module].Module_Subfolders[a];
+            if (Object.Subfolder_Status == "Active"){
+                Explorer_Item_Create(Object.Subfolder_ID, Object.Subfolder_Name, Object.Subfolder_LastModified, null, Object.Subfolder_Link, null);
+            }
         }
-    }
+        
+        document.getElementById("Quizzes_Title_Folder").innerHTML = Quizzes_Manifest.Subject[Item].Subject_Module[Module].Module_Name;
+        Element_Style_Animate_Batch_QuerySelector(".Explorer_Item", "Explorer_Item_Open", "0.5s", "forwards", "1", 100);
+    }, 200);
 }
 
 // Goes up a level
 function Explorer_Back(){
-    Explorer_Container_Clear();
-    if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "1"){
-        Quizzes_Explorer_Load_Subject();
-    }
-    if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "2"){
-        Quizzes_Explorer_Load_Subject();
-    }
-    if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "3"){
-        Quizzes_Explorer_Load_Module(Element_Attribute_Get("Quizzes_Explorer", "Memory_Value"));
-    }
+    
+        if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "1"){
+            Quizzes_Explorer_Load_Subject();
+        }
+        if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "2"){
+            Quizzes_Explorer_Load_Subject();
+        }
+        if (Element_Attribute_Get("Quizzes_Explorer", "Level") == "3"){
+            Quizzes_Explorer_Load_Module(Element_Attribute_Get("Quizzes_Explorer", "Memory_Value"));
+        }
+    
+    
 }
 
 // Clears the contents of the container
